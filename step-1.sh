53693b49a6d7f10cb7e6d68e75762c7c95d34c56
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-23 00:05:43 +0530

    Update step-1.sh

--- step-1.sh
+++ step-1.sh
@@ -4,15 +4,8 @@
 #(Execute in Master Node:) 
 
 sudo apt update      
-
 sudo snap install microk8s --classic
-
 sudo microk8s.status
-
 microk8s.kubectl get nodes
-
 sudo snap alias microk8s.kubectl kubectl
-
 microk8s add-node
-
-
