9e7b56ba63ae53d5a5280701af34b85a3c3f5180
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-23 00:07:35 +0530

    Update step-1.sh

--- step-1.sh
+++ step-1.sh
@@ -3,9 +3,10 @@
 
 #(Execute in Master Node:) 
 
-sudo apt update      
-sudo snap install microk8s --classic
-sudo microk8s.status
-microk8s.kubectl get nodes
-sudo snap alias microk8s.kubectl kubectl
-microk8s add-node
+sudo apt update                            # Update the apt package
+sudo snap install microk8s --classic       # install microK8s through snap
+sudo microk8s.status                       # check the running status of microK8s
+microk8s.kubectl get nodes                 # Check the node status
+sudo snap alias microk8s.kubectl kubectl   # make alias of microk8s.kubectl to kubectl
+microk8s add-node                          # fire the comman to get the join command for worker node 
+
