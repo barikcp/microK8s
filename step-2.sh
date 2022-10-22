00f8358df5618dce7dccd5fa586d3dea0129d054
barikcp <113718872+barikcp@users.noreply.github.com>
2022-10-23 00:12:41 +0530

    Update step-2.sh

--- step-2.sh
+++ step-2.sh
@@ -1,15 +1,8 @@
 
 
+#(Execute in worker-node-2)
 
-
-
-#(Execute in worker-node-1)
-
-sudo apt update
-
-sudo snap install microk8s --classic
-
-sudo microk8s.status  # Once this is showing as running, run below join command shown in master-node output
-
-microk8s join 10.160.0.31:25000/4f6b92e353c427a0a305d383fe50d58a/42a5dc2f564e  # run "microk8s add-node" inside master node to get this command
-
+sudo apt update                              # Update the apt package            
+sudo snap install microk8s --classic         # install microK8s through snap
+sudo microk8s.status                         # Once this is showing as running, run below join command shown in master-node output
+microk8s join <TOKEN_SHOWN_IN_ADD-NODE_COMMAND_IN_MASTER_NODE>  # run "microk8s add-node" inside master node to get this command
